70af3b133b3b3a806c87a76edada80bc21c3c2ae
Dariusssssssss <sablezzubaya@gmail.com>
2025-10-27 21:00:34 +0300

    add: 15/37

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -282,8 +282,12 @@ function getFibonacciNumber(index) {
  *   10 => 55 // (1+2+3+...+10)
  *   1  => 1
  */
-function getSumToN(/* n */) {
-  throw new Error('Not implemented');
+function getSumToN(n) {
+  let count = 0;
+  for (let i = 0; i <= n; i += 1) {
+    count += i;
+  }
+  return count;
 }
 
 /**
